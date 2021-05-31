653a66081f9c8968fa2039fce7a69ebd7d7787d3
iamjongholee <jongho0530@gmail.com>
2021-06-01 00:15:41 +0900

    Calculator Program 과제제출

---
+++ CalculatorProject/src/main/java/BackwardOper/Operator/NumOperand.java
@@ -0,0 +1,18 @@
+package BackwardOper.Operator;
+/*
+* 피연산자 클래스.
+*/
+
+public class NumOperand extends ABS_CalMember
+{// 숫자 피연산자.
+	private int value;
+	public NumOperand(int v)
+	{
+		this.value = v;
+		}
+	public int getValue()
+	{
+		return this.value;
+		}
+	@Override public String toString() { return String.valueOf(this.value); }
+	}
